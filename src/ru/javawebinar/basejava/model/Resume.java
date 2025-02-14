c403a357f09cd23c53e3ed340fb8c3f51238799d
r0maniast <romankrivtsov7@gmail.com>
2025-02-14 15:49:38 +0300

    Null point

---
+++ src/ru/javawebinar/basejava/model/Resume.java
@@ -0,0 +1,45 @@
+package ru.javawebinar.basejava.model;
+
+import java.util.Objects;
+
+/**
+ * Initial resume class
+ */
+public class Resume implements Comparable<Resume> {
+
+    // Unique identifier
+    private String uuid;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Resume resume = (Resume) o;
+
+        return Objects.equals(uuid, resume.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return uuid != null ? uuid.hashCode() : 0;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    @Override
+    public String toString() {
+        return uuid;
+    }
+
+    @Override
+    public int compareTo(Resume o) {
+        return uuid.compareTo(o.uuid);
+    }
+}
---
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -0,0 +1,30 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.model.Resume;
+
+import java.util.Arrays;
+
+public class SortedArrayStorage extends AbstractArrayStorage {
+
+    @Override
+    protected void insertElement(Resume r, int index) {
+        index = -index - 1;
+        System.arraycopy(storage, index, storage, index + 1, size - index);
+        storage[index] = r;
+    }
+
+    @Override
+    protected void fillDeletedElement(int index) {
+        int numMoved = size - (index + 1);
+        if (numMoved > 0) {
+            System.arraycopy(storage, index + 1, storage, index, numMoved);
+        }
+    }
+
+    @Override
+    protected int getIndex(String uuid) {
+        Resume searchKey = new Resume();
+        searchKey.setUuid(uuid);
+        return Arrays.binarySearch(storage, 0, size, searchKey);
+    }
+}
