ef4a5f068ebbed350165cf8a0aebfce3c5e7db14
r0maniast <romankrivtsov7@gmail.com>
2025-04-01 20:28:43 +0300

    Test Refactoring

---
+++ src/test/java/ru/basejava/storage/AllStorageTest.java
@@ -0,0 +1,14 @@
+package ru.basejava.storage;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({
+        ArrayStorageTest.class,
+        SortedArrayStorageTest.class,
+        ListStorageTest.class,
+        MapUuidStorageTest.class,
+})
+public class AllStorageTest {
+}
